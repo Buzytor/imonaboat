b517351c2f7480ea5c000d5b310574b2c0bc8a30
Michal Blotniak <buoto@buzytor.com>
2015-01-24 03:19:01 +0100

    Add wobbler

--- Assets/PlayerShipBehaviour.cs
+++ Assets/PlayerShipBehaviour.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class PlayerShipBehaviour : MonoBehaviour {
 
+   
     public float speed = 1.0f;
     public Vector3 velocity;
+
+    Wobbler wob = new Wobbler();
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +16,13 @@ public class PlayerShipBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        float w = wob.GetWobWob();
+        Debug.Log(w);
+        transform.Translate(Vector3.up*w*Time.deltaTime);
 	}
+
+    void OnTrigerEnter(Collider collider) {
+        // TODO failure state
+        Debug.Log("Collision!");
+    }
 }
---
+++ Assets/Wobbler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Wobbler {
+    public float maxYWobbble = 0.5f;
+    public int wobblePhases = 30;
+    private float YWobbble = 0;
+    private bool directionUp = true;
+    private float alpha = 0;
+
+    public float GetWobWob() {
+        alpha += (Mathf.PI/wobblePhases) % (2* Mathf.PI);
+        float wobbleStep = maxYWobbble * Mathf.Sin(alpha);
+        if(YWobbble + wobbleStep > maxYWobbble) {
+            directionUp = false;
+        } else if(YWobbble - wobbleStep < -maxYWobbble) {
+            directionUp = true;
+        }
+        return wobbleStep;
+    }
+
+}
