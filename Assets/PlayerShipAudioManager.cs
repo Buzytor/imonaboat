69bbecdc8eddbf1e06b8acc2052b21e0256b82a1
Michal Blotniak <buoto@buzytor.com>
2015-01-25 11:48:39 +0100

    Remove horn start playing cause it's buggy as hell

--- Assets/PlayerShipAudioManager.cs
+++ Assets/PlayerShipAudioManager.cs
@@ -3,22 +3,18 @@ using System.Collections;
 
 public class PlayerShipAudioManager : MonoBehaviour {
     public AudioSource audioSource;
-    public AudioClip hornStart;
     public AudioClip hornEnd;
     public AudioClip hornLoop;
 
     
 
     internal void StartHorn() {
-        audioSource.Stop();
         audioSource.clip = hornLoop;
-        audioSource.PlayDelayed(hornStart.length);
-        audioSource.PlayOneShot(hornStart);
+        audioSource.Play();
         audioSource.loop = true;
     }
 
     internal void StopHorn() {
-        audioSource.clip = null;
         audioSource.Stop();
         audioSource.PlayOneShot(hornEnd);
     }
